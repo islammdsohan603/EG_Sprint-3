bbe7f96a920468bff74a93566d5da3abbeb0a8a2
islammdsohan603 <islammdsohan603@gmail.com>
2026-09-04 16:38:46 +0600

    anagram valid problam solve

--- EG-13_Sprint-3_PS.js
+++ EG-13_Sprint-3_PS.js
@@ -1,22 +1,13 @@
-// Move Zeroes
 
+// valid anagram
 
-var moveZeroes = function (nums) {
-  let index = 0;
-
-  for (let i = 0; i < nums.length; i++) {
-    if (nums[i] !== 0) {
-      nums[index] = nums[i];
-      index++;
-    }
+var isAnagram = function (s, t) {
+  if (s.length !== t.length) {
+    return false;
   }
-
-  while (index < nums.length) {
-    nums[index] = 0;
-    index++;
-  }
-
-  return nums;
+  const sortedS = s.split('').sort().join('');
+  const sortedT = t.split('').sort().join('');
+  return sortedS === sortedT;
 }
 
-console.log(moveZeroes([0, 1, 0, 3, 12]))
+console.log(isAnagram("anagram", "nagaram"));
